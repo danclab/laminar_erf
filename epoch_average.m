326364060eb170c4e145a2ab812bc19fd0419ad7
jbonaiuto <jbonaiuto@gmail.com>
2022-02-14 14:11:58 +0100

    don't use robust averaging

--- epoch_average.m
+++ epoch_average.m
@@ -8,19 +8,8 @@ clear jobs
 matlabbatch={};
 
 matlabbatch{1}.spm.meeg.averaging.average.D = {fname};
-matlabbatch{1}.spm.meeg.averaging.average.userobust.robust.ks = 3;
-matlabbatch{1}.spm.meeg.averaging.average.userobust.robust.bycondition = false;
-matlabbatch{1}.spm.meeg.averaging.average.userobust.robust.savew = false;
-matlabbatch{1}.spm.meeg.averaging.average.userobust.robust.removebad = false;
+matlabbatch{1}.spm.meeg.averaging.average.userobust.standard = false;
 matlabbatch{1}.spm.meeg.averaging.average.plv = false;
 matlabbatch{1}.spm.meeg.averaging.average.prefix = 'm';
-matlabbatch{2}.spm.meeg.preproc.filter.D(1) = cfg_dep('Averaging: Averaged Datafile', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','Dfname'));
-matlabbatch{2}.spm.meeg.preproc.filter.type = 'butterworth';
-matlabbatch{2}.spm.meeg.preproc.filter.band = 'low';
-matlabbatch{2}.spm.meeg.preproc.filter.freq = 120;
-matlabbatch{2}.spm.meeg.preproc.filter.dir = 'twopass';
-matlabbatch{2}.spm.meeg.preproc.filter.order = 5;
-matlabbatch{2}.spm.meeg.preproc.filter.prefix = 'f';
-
 
 spm_jobman('run', matlabbatch);    
